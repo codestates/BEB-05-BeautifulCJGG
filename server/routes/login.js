dd3d44e3ae7a2524c2f7efb0edd61400f09b74b2
BrilliantCB <parkaet@gmail.com>
2022-08-19 18:30:16 +0900

    init

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 const router = Router();
 import { controller } from './../controllers/index.js';
 
-router.get('/', controller.metadata.get);
-router.post('/', controller.metadata.post);
+router.get('/', controller.login.get);
+router.post('/', controller.login.post);
 
 export default router;
