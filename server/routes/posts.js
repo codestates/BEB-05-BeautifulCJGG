586eec3b376b428b19e8bb47ab2e32d8dd5af31c
BrilliantCB <parkaet@gmail.com>
2022-08-22 09:08:48 +0900

    first push

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 const router = Router();
 import { getPost, getPosts } from './../controllers/postController.js';
 
-console.log("posts.js")
+//console.log("posts.js")
 
 router.get('/', getPosts);
 router.get('/:id/', getPost);
