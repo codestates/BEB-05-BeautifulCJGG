84b896d9f5978e39055739faebb3d6c70b36acb4
Moshed-Potato <rudah03@naver.com>
2022-08-22 14:34:05 +0900

    병합 충돌 해결

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './assets/css/index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
