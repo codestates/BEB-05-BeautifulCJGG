89f3a13d426de3280b85d72a3669b2683ba5f111
kimsj3592 <kimsj3592@naver.com>
2022-08-24 15:29:46 +0900

    login,signup page 완성

--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -1,19 +1,22 @@
-import React, { useState } from 'react';
-import signup from '../apis/signup';
+import React, { useState } from "react";
+import signup from "../apis/signup";
+import styles from "../assets/css/SignUp.module.css";
 
 export default function SignupPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const onChange = (event) => {
-    const {target: {name, value}} = event;
-    if(name === 'email'){
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
       setEmail(value);
-    } else if(name === 'password'){
+    } else if (name === "password") {
       setPassword(value);
     }
-  }
+  };
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -22,18 +25,39 @@ export default function SignupPage() {
   };
 
   return (
-    <div>
-      <div>
-        <h2>Sign up</h2>
-        <form onSubmit={onSubmit}>
-          <input name='email' type='text' placeholder='Email' required value={email} onChange={onChange}></input>
-          <input name='password' type='password' placeholder='Password' required value={password} onChange={onChange}></input>
-          <input type='submit' value="Sign up"></input>
-        </form>
+    <div className={`${styles.signup}`}>
+      <div className={`${styles.signup_box}`}>
+        <div className={`${styles.left}`}>
+          <div className={`${styles.contact}`}>
+            <form onSubmit={onSubmit}>
+              <h2>Sign Up</h2>
+              <input
+                name="email"
+                type="text"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={onChange}
+              ></input>
+              <input
+                name="password"
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={onChange}
+              ></input>
+              <input type="submit" value="Sign Up"></input>
+            </form>
+          </div>
+        </div>
+        <div className={`${styles.right}`}>
+          <div className={`${styles.right_text}`}>
+            <h2>BeautifulCJGG</h2>
+          </div>
+        </div>
       </div>
-      <p>
-        {error}
-      </p>
+      <p>{error}</p>
     </div>
   );
 }
